98c71d15c6ef5ef842687f3d4629cd1d53007f3c
F-loat <chaimaoyuan@foxmail.com>
2020-04-25 12:16:04 +0800

    feat: 初始功能完成

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+
+program
+  .command('parse [htmlfile]')
+  .description('解析 HTML 文件')
+  .action(file => {
+    require('../lib/parse')(file)
+  })
+
+program
+  .command('render [htmlfile] [textsfile]')
+  .description('生成 HTML 文件')
+  .action((htmlfile, textsfile) => {
+    require('../lib/render')(htmlfile, textsfile)
+  })
+
+program
+  .command('translate [textsfile]')
+  .description('翻译 JSON 文件')
+  .action(file => {
+    require('../lib/translate')(file)
+  })
+
+program.parse(process.argv)
